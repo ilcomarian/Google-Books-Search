115121a5a0d159099ae8ab4539243f1d69c6d563
ilcomarian <ilcomarian@gmail.com>
2018-08-03 22:20:09 -0400

    finish

--- server.rb
+++ server.rb
@@ -8,20 +8,15 @@ get "/" do
    end
 
  post "/" do
-  @select = params[:query]
-  books = GoogleBooks.search(@select)
-  first_book = books.first
-  @author = first_book.authors
-   @img =  first_book.image_link(:curl => true)
-   @info = first_book.info_link
-   @title = first_book.title
-   erb :show
-   end
+    @select = params[:query]
+    books = GoogleBooks.search(@select)
+    first_book = books.first
+    @author = first_book.authors
+    @img =  first_book.image_link(:curl => true)
+    @info = first_book.info_link
+    @title = first_book.title
+    erb :show
+  end
  
 
-#  books = GoogleBooks.search('The Great Gatsby')
-# first_book = books.first
-# "#{first_book.authors}"
-#  @author = first_book.authors
-#  @img =  first_book.image_link
-#  erb :show
+
